da598b86e5a1cb0fb63eeb90ca84fb27a2323041
AdarshMitr <adarshkumar019@gmail.com>
2024-01-12 21:46:25 +0530

    fetching data from API for single product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-  return (
-    <div>Our Products</div>
-  )
-}
+  const [data, setData] = useState(['']);
 
-export default Products
+  async function getList() {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const itemList = await response.json();
+   
+    setData(itemList);
+    console.log(itemList)
+  }
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  let {id, title, price, description, category, image, rating}=data ;
+  return (
+    <div className="cardContainer">
+      <div className="itemCard">
+        <div className="category">{data[0].category}</div>
+        <img  className="img" src={data[0].image} alt={title}/>
+        <span>₹{data[0].price}</span>
+       
+        <div className="title" id={2}>
+        {data[0].title}
+        </div>
+        
+        
+        
+      </div>
+    </div>
+  );
+};
+
+export default Products;
